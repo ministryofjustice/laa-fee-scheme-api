4ce455683ba6dac2e7144a5bf0e9d1545109462d
mukesh-moj <mukesh.kumar@justice.gov.uk>
2025-07-28 23:23:07 +0100

    db scripts modified

--- scheme-service/src/main/resources/db/migration/V1__init_schema.sql
+++ scheme-service/src/main/resources/db/migration/V1__init_schema.sql
@@ -1,34 +1,56 @@
--- Drop the table if it exists
 DROP TABLE IF EXISTS fee CASCADE;
 
--- Drop the table if it exists
 DROP TABLE IF EXISTS fee_schemes CASCADE;
 
+DROP TABLE IF EXISTS categories CASCADE;
 
-CREATE TABLE IF NOT EXISTS fee_schemes (
-                                           fee_schemes_id  SERIAL PRIMARY KEY,
-                                           fee_scheme_code varchar UNIQUE NOT NULL,
-                                           "name"          varchar        NOT NULL,
-                                           valid_from      date           NOT NULL,
-                                           valid_to        date           NULL
+DROP TABLE IF EXISTS category_bolt_on_fees CASCADE;
+
+DROP TABLE IF EXISTS legal_help_categories CASCADE;
+
+CREATE TABLE IF NOT EXISTS fee_schemes
+(
+    scheme_code TEXT PRIMARY KEY,
+    scheme_name varchar NOT NULL,
+    valid_from  date    NOT NULL,
+    valid_to    date    NULL
 );
 
 
-CREATE TABLE IF NOT EXISTS fee (
-                                   fee_id                      SERIAL PRIMARY KEY,
-                                   fee_code                    varchar UNIQUE NOT NULL,
-                                   fee_schemes_id              int8           NOT NULL,
-                                   total_fee                   numeric        NULL,
-                                   profit_cost_limit           numeric        NULL,
-                                   disbursement_limit          numeric        NULL,
-                                   prior_authority_applicable  bool           NULL,
-                                   ho_interview_bolt_on        varchar        NULL,
-                                   oral_cmrh_bolt_on           varchar        NULL,
-                                   telephone_cmrh_bolt_on      varchar        NULL,
-                                   substantive_hearing_bolt_on varchar        NULL,
-                                   adjorn_hearing_bolt_on      varchar        NULL,
-                                   schedule_reference          varchar        NULL
+CREATE TABLE IF NOT EXISTS fee
+(
+    fee_id                     SERIAL PRIMARY KEY,
+    fee_code                   varchar UNIQUE NOT NULL,
+    fee_scheme_code            TEXT           NOT NULL REFERENCES fee_schemes (scheme_code),
+    total_fee                  numeric        NULL,
+    profit_cost_limit          numeric        NULL,
+    disbursement_limit         numeric        NULL,
+    prior_authority_applicable bool           NULL,
+    schedule_reference         bool           NULL
 );
 
+CREATE TABLE IF NOT EXISTS categories
+(
+    category_code TEXT PRIMARY KEY,
+    category_name varchar NOT NULL
+);
 
-ALTER TABLE fee ADD CONSTRAINT fee_fee_schemes_fk FOREIGN KEY (fee_schemes_id) REFERENCES fee_schemes(fee_schemes_id);
+CREATE TABLE IF NOT EXISTS category_bolt_on_fees
+(
+    id                          SERIAL PRIMARY KEY,
+    category_code               TEXT    NOT NULL REFERENCES categories (category_code),
+    ho_interview_bolt_on        numeric NULL,
+    oral_cmrh_bolt_on           numeric NULL,
+    telephone_cmrh_bolt_on      numeric NULL,
+    substantive_hearing_bolt_on numeric NULL,
+    adjorn_hearing_bolt_on      numeric NULL
+);
+
+CREATE TABLE IF NOT EXISTS legal_help_categories
+(
+    id               SERIAL PRIMARY KEY,
+    category_code    TEXT         NOT NULL REFERENCES categories (category_code),
+    full_description VARCHAR(255) NOT NULL,
+    area_of_law      VARCHAR(50) DEFAULT 'Legal Help (Civil)'
+);
+
