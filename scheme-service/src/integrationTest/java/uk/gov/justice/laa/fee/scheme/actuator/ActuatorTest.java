d96b707ec4bae0dd2614416ae420be01b330712b
Tosin <tosin.yusuf@digital.justice.gov.uk>
2025-08-06 12:22:34 +0100

    LFSP-60: Tidy up

--- scheme-service/src/integrationTest/java/uk/gov/justice/laa/fee/scheme/actuator/ActuatorTest.java
+++ scheme-service/src/integrationTest/java/uk/gov/justice/laa/fee/scheme/actuator/ActuatorTest.java
@@ -2,7 +2,6 @@ package uk.gov.justice.laa.fee.scheme.actuator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
