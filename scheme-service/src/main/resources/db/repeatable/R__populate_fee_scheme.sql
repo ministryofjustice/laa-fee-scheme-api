1b5ad451139f55a27bf787afb575a096057839fc
drummondjm <mark.drummond@justice.gov.uk>
2025-08-06 10:48:26 +0100

    update for new structure

--- scheme-service/src/main/resources/db/repeatable/R__populate_fee_scheme.sql
+++ scheme-service/src/main/resources/db/repeatable/R__populate_fee_scheme.sql
@@ -1,3 +1,3 @@
-INSERT INTO "fee-scheme".fee_schemes (scheme_code, scheme_name, valid_from, valid_to)
+INSERT INTO fee_schemes (scheme_code, scheme_name, valid_from, valid_to)
 VALUES
     ('MED_FS2013', 'mediation fee scheme 2013', '2013-04-01', NULL);
--- scheme-service/src/main/resources/db/repeatable/R__populate_mediation_fee_data.sql
+++ scheme-service/src/main/resources/db/repeatable/R__populate_mediation_fee_data.sql
@@ -1,52 +1,54 @@
-INSERT INTO "fee-scheme".fee (fee_code, total_fee, mediation_session_one, mediation_session_two, fee_scheme_code)
+INSERT INTO fee (fee_code, description, total_fee, mediation_session_one, mediation_session_two, fee_scheme_code)
 VALUES
-    ('MAM1', 87.00, NULL, NULL, 'MED_FS2013'),
-    ('MAM2', 87.00, NULL, NULL, 'MED_FS2013'),
-    ('MAM3', 130.00, NULL, NULL, 'MED_FS2013'),
-    ('MED1', NULL, 168.00, 756.00, 'MED_FS2013'),
-    ('MED2', NULL, 168.00, 462.00, 'MED_FS2013'),
-    ('MED3', NULL, 230.00, 1064.00, 'MED_FS2013'),
-    ('MED4', NULL, 230.00, 647.00, 'MED_FS2013'),
-    ('MED5', NULL, 420.00, 1008.00, 'MED_FS2013'),
-    ('MED6', NULL, 294.00, 588.00,  'MED_FS2013'),
-    ('MED7', NULL, 357.00, 945.00,  'MED_FS2013'),
-    ('MED8', NULL, 262.50, 556.50,  'MED_FS2013'),
-    ('MED9', NULL, 294.00, 588.00,  'MED_FS2013'),
-    ('MED10', NULL, 231.00, 525.00,  'MED_FS2013'),
-    ('MED11', NULL, 482.00, 1316.00,  'MED_FS2013'),
-    ('MED12', NULL, 356.00, 773.00,  'MED_FS2013'),
-    ('MED13', NULL, 419.00, 1253.00,  'MED_FS2013'),
-    ('MED14', NULL, 324.50, 741.50,  'MED_FS2013');
 
-INSERT INTO "fee-scheme".fee (fee_code, total_fee, mediation_session_one, mediation_session_two, fee_scheme_code)
+    ('MAM1', 'Mediation Assesment (alone)', 87.00, NULL, NULL, 'MED_FS2013'),
+    ('MAM2', 'Mediation Assesment (separate)', 87.00, NULL, NULL, 'MED_FS2013'),
+    ('MAM3', 'Mediation Assesment (together)', 130.00, NULL, NULL, 'MED_FS2013'),
+    ('MED1', 'All Issues sole -  2 parties eligible, no agreement', NULL, 168.00, 756.00, 'MED_FS2013'),
+    ('MED2', 'All Issues sole - 1 party eligible, no agreement', NULL, 168.00, 462.00, 'MED_FS2013'),
+    ('MED3', 'All Issues Co - 2 parties eligible, no agreement', NULL, 230.00, 1064.00, 'MED_FS2013'),
+    ('MED4', 'All Issues Co -1 party eligible, no agreement', NULL, 230.00, 647.00, 'MED_FS2013'),
+    ('MED5', 'All issues sole -  2 parties eligible - Agreement on all issues', NULL, 420.00, 1008.00, 'MED_FS2013'),
+    ('MED6', 'All issues sole -  1 party eligible - Agreement on all issues', NULL, 294.00, 588.00, 'MED_FS2013'),
+    ('MED7', 'All issues sole - 2 parties eligible, agreement on P&F only', NULL, 357.00, 945.00, 'MED_FS2013'),
+    ('MED8', 'All issues sole -  1 party eligible, agreement on P&F only', NULL, 262.50, 556.50, 'MED_FS2013'),
+    ('MED9', 'All issues sole - 2 parties eligible, agreement on Children only ', NULL, 294.00, 588.00, 'MED_FS2013'),
+    ('MED10', 'All issues sole - 1 party eligible, agreement on Children only ', NULL, 231.00, 525.00, 'MED_FS2013'),
+    ('MED11', 'All issues Co-Mediation -  2 parties eligible - Agreement on all issues', NULL, 482.00, 1316.00, 'MED_FS2013'),
+    ('MED12', 'All issues co-mediation -  1 party eligible - Agreement on all issues', NULL, 356.00, 773.00, 'MED_FS2013'),
+    ('MED13', 'All issues co-mediation - 2 parties eligible, agreement on P&F only', NULL, 419.00, 1253.00, 'MED_FS2013'),
+    ('MED14', 'All issues co-mediation - 1 party eligible, agreement on P&F only', NULL, 324.50, 741.50, 'MED_FS2013'),
+    ('MED15', 'All issues co-mediation - 2 parties eligible, agreement on Children only ', NULL, 356.00, 1190.00, 'MED_FS2013');
+
+INSERT INTO fee (fee_code, description, total_fee, mediation_session_one, mediation_session_two, fee_scheme_code)
 VALUES
-    ('MED15', NULL, 356.00, 1190.00,  'MED_FS2013'),
-    ('MED16', NULL, 293.00, 710.00,  'MED_FS2013'),
-    ('MED17', NULL, 168.00, 588.00,  'MED_FS2013'),
-    ('MED18', NULL, 168.00, 378.00,  'MED_FS2013'),
-    ('MED19', NULL, 230.00, 834.00,  'MED_FS2013'),
-    ('MED20', NULL, 230.00, 532.00,  'MED_FS2013'),
-    ('MED21', NULL, 168.00, 651.00,  'MED_FS2013'),
-    ('MED22', NULL, 168.00, 378.00,  'MED_FS2013'),
-    ('MED23', NULL, 230.00, 834.00,  'MED_FS2013'),
-    ('MED24', NULL, 230.00, 721.00,  'MED_FS2013'),
-    ('MED25', NULL, 168.00, 462.00,  'MED_FS2013'),
-    ('MED26', NULL, 168.00, 315.00,  'MED_FS2013'),
-    ('MED27', NULL, 230.00, 647.00,  'MED_FS2013'),
-    ('MED28', NULL, 230.00, 438.50,  'MED_FS2013'),
-    ('MED29', NULL, 294.00, 588.00,  'MED_FS2013'),
-    ('MED30', NULL, 231.00, 378.00,  'MED_FS2013'),
-    ('MED31', NULL, 356.00, 773.00,  'MED_FS2013'),
-    ('MED32', NULL, 293.00, 501.50,  'MED_FS2013');
+    ('MED16', 'All issues co-mediation -  1 party eligible, agreement on Children only ', NULL, 293.00, 710.00, 'MED_FS2013'),
+    ('MED17', 'Property & Finance sole -  2 parties eligible, no agreement', NULL, 168.00, 588.00, 'MED_FS2013'),
+    ('MED18', 'Property & Finance sole -  1 party eligible, no agreement', NULL, 168.00, 378.00, 'MED_FS2013'),
+    ('MED19', 'Property & Finance Co - 2 parties eligible, no agreement', NULL, 230.00, 834.00, 'MED_FS2013'),
+    ('MED20', 'Property & Finance Co - 1 party eligible, no agreement', NULL, 230.00, 532.00, 'MED_FS2013'),
+    ('MED21', 'Property & Finance sole -  2 parties eligible, with agreed proposal', NULL, 168.00, 651.00, 'MED_FS2013'),
+    ('MED22', 'Property & Finance sole -1 party eligible, with agreed proposal', NULL, 168.00, 378.00, 'MED_FS2013'),
+    ('MED23', 'Property & Finance Co -  2 parties eligible, with agreed proposal', NULL, 230.00, 834.00, 'MED_FS2013'),
+    ('MED24', 'Property & Finance Co -  1 party eligible, with agreed proposal', NULL, 230.00, 721.00, 'MED_FS2013'),
+    ('MED25', 'Child only sole -2 parties eligible, no agreement', NULL, 168.00, 462.00, 'MED_FS2013'),
+    ('MED26', 'Child only sole - 1 party eligible, no agreement', NULL, 168.00, 315.00, 'MED_FS2013'),
+    ('MED27', 'Child only Co - 2 parties eligible, no agreement', NULL, 230.00, 647.00, 'MED_FS2013'),
+    ('MED28', 'Child only Co -  1 party eligible, no agreement', NULL, 230.00, 438.50, 'MED_FS2013'),
+    ('MED29', 'Child only sole - single session, 2 parties eligible, with agreed proposal', NULL, 294.00, 588.00, 'MED_FS2013'),
+    ('MED30', 'Child only sole - single session 1 party eligible, with agreed proposal', NULL, 231.00, 378.00, 'MED_FS2013'),
+    ('MED31', 'Child only Co - single session 2 parties eligible, with agreed proposal', NULL, 356.00, 773.00, 'MED_FS2013'),
+    ('MED32', 'Child only Co - single session 1 party eligible, with agreed proposal', NULL, 293.00, 501.50, 'MED_FS2013');
 
---
--- INSERT INTO "fee-scheme".fee (fee_code, total_fee, mediation_session_one, mediation_session_two, fee_scheme_code)
+
+-- INSERT INTO fee (fee_code, description, total_fee, mediation_session_one, mediation_session_two, fee_scheme_code)
 -- VALUES
 --
 --     \\ Use If existing data needs modified \\
 --
 --     ON CONFLICT (fee_code) DO UPDATE SET
---     total_fee = EXCLUDED.total_fee,
---                                   mediation_session_one = EXCLUDED.mediation_session_one,
---                                   mediation_session_two = EXCLUDED.mediation_session_two,
---                                   fee_scheme_code = EXCLUDED.fee_scheme_code;
+--         total_fee = EXCLUDED.total_fee,
+--         description = EXCLUDED.description,
+--         mediation_session_one = EXCLUDED.mediation_session_one,
+--         mediation_session_two = EXCLUDED.mediation_session_two,
+--         fee_scheme_code = EXCLUDED.fee_scheme_code;
