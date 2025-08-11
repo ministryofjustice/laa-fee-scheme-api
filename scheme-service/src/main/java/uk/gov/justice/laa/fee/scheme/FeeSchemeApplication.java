ffa53aa73fcf328bbc75418ed0bc24c7de31722f
mukesh-moj <mukesh.kumar@justice.gov.uk>
2025-08-11 12:23:58 +0100

    files modified for testing purpose

--- scheme-service/src/main/java/uk/gov/justice/laa/fee/scheme/FeeSchemeApplication.java
+++ scheme-service/src/main/java/uk/gov/justice/laa/fee/scheme/FeeSchemeApplication.java
@@ -1,6 +1,7 @@
 package uk.gov.justice.laa.fee.scheme;
 
 import io.sentry.Sentry;
+import io.sentry.SentryLevel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -18,5 +19,6 @@ public class FeeSchemeApplication {
   public static void main(String[] args) {
     SpringApplication.run(FeeSchemeApplication.class, args);
     Sentry.captureMessage("Sentry is configured successfully for LAA-Fee-Scheme Application!");
+    Sentry.captureMessage("FATAL-Sentry is configured successfully for LAA-Fee-Scheme Application!", SentryLevel.FATAL);
   }
 }
