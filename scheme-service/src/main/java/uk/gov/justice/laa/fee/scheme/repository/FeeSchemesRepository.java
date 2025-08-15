aeb0f29dd41130fea4fd9627b83b63d40f4d6eb2
drummondjm <mark.drummond@justice.gov.uk>
2025-08-15 09:42:44 +0100

    calculation logic for determining mediation fees

--- scheme-service/src/main/java/uk/gov/justice/laa/fee/scheme/repository/FeeSchemesRepository.java
+++ scheme-service/src/main/java/uk/gov/justice/laa/fee/scheme/repository/FeeSchemesRepository.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import uk.gov.justice.laa.fee.scheme.entity.FeeSchemesEntity;
 
@@ -14,14 +15,15 @@ import uk.gov.justice.laa.fee.scheme.entity.FeeSchemesEntity;
 public interface FeeSchemesRepository extends JpaRepository<FeeSchemesEntity, String> {
 
   @Query("""
-        SELECT fs
-        FROM FeeEntity f
-        JOIN f.feeSchemeCode fs
-        WHERE f.feeCode = :feeCode
-          AND fs.validFrom <= :inputDate
-          AND (fs.validTo IS NULL OR fs.validTo >= :inputDate)
-        ORDER BY fs.validFrom DESC
-        """)
-  Optional<FeeSchemesEntity> findValidSchemeForDate(String feeCode, LocalDate inputDate);
+      SELECT scheme
+      FROM FeeEntity fee
+           JOIN fee.feeSchemeCode scheme
+      WHERE fee.feeCode = :feeCode
+        AND scheme.validFrom <= :inputDate
+        AND (scheme.validTo IS NULL OR scheme.validTo >= :inputDate)
+      ORDER BY scheme.validFrom DESC
+      """)
+  Optional<FeeSchemesEntity> findValidSchemeForDate(@Param("feeCode") String feeCode,
+                                                    @Param("inputDate") LocalDate inputDate);
 }
 
