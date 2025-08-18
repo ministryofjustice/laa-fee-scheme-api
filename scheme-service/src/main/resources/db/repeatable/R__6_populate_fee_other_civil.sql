6b0dd116ca93ad58b2f60e2fc95df9827378967b
Tosin <tosin.yusuf@justice.gov.uk>
2025-08-18 10:54:02 +0100

    LFSP-63: Populate community care fee data (#41)
    * LFSP-63: Add initial fee data
    
    * LFSP-63: Updated scripts
    
    * LFSP-63: Tidy up
    
    * LFSP-63: Fixed test

---
+++ scheme-service/src/main/resources/db/repeatable/R__6_populate_fee_other_civil.sql
@@ -0,0 +1,3 @@
+INSERT INTO fee (fee_code, description, total_fee, escape_threshold_limit, fee_scheme_code, calculation_type)
+VALUES ('COM', 'Community Care Legal Help Fixed Fee', 266.00,  798.00, 'COM_FS2013', 'COMMUNITY_CARE')
+ON CONFLICT (fee_code, fee_scheme_code) DO NOTHING;
