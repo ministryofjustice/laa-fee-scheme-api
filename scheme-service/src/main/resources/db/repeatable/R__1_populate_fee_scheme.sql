ce5e0e1b33f61d3edc6312e08acba93f801eb497
Tosin <tosin.yusuf@digital.justice.gov.uk>
2025-08-18 10:27:57 +0100

    LFSP-63: Tidy up

--- scheme-service/src/main/resources/db/repeatable/R__1_populate_fee_scheme.sql
+++ scheme-service/src/main/resources/db/repeatable/R__1_populate_fee_scheme.sql
@@ -13,7 +13,7 @@ VALUES
     ('MED_FS2013', 'Mediation Fee Scheme 2013', '2013-04-01', NULL)
 ON CONFLICT (scheme_code) DO NOTHING;
 
--- Fee Scheme for 'Other Civil Cases' category
+-- Fee Scheme for 'Community Care' category
 INSERT INTO fee_schemes (scheme_code, scheme_name, valid_from, valid_to)
 VALUES
     ('COM_FS2013', 'Community Care Fee Scheme 2013', '2013-04-01', NULL) -- awaiting scheme date
