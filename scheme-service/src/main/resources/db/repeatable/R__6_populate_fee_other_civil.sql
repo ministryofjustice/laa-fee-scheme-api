3a8546526d5f56880cc78f54fe644236447c1c2c
Tosin <tosin.yusuf@digital.justice.gov.uk>
2025-08-18 10:17:09 +0100

    LFSP-63: Updated scripts

--- scheme-service/src/main/resources/db/repeatable/R__6_populate_fee_other_civil.sql
+++ scheme-service/src/main/resources/db/repeatable/R__6_populate_fee_other_civil.sql
@@ -1,3 +1,3 @@
 INSERT INTO fee (fee_code, description, total_fee, escape_threshold_limit, fee_scheme_code, calculation_type)
-VALUES ('COM', 'Community Care Legal Help Fixed Fee', 266.00,  798.00, 'COM_FS2000', 'OTHER_CIVIL_CASES')
+VALUES ('COM', 'Community Care Legal Help Fixed Fee', 266.00,  798.00, 'COM_FS2013', 'OTHER_CIVIL')
 ON CONFLICT (fee_code, fee_scheme_code) DO NOTHING;
