5220472f30cc24992b870cf9a389e25720b05b5a
Tosin <tosin.yusuf@justice.gov.uk>
2025-08-19 17:20:46 +0100

    LFSP-64: Populate other civil fee data (#47)
    * LFSP-64: Populate Other Civil categories fee data
    
    * LFSP-64: Tidy up

--- scheme-service/src/main/resources/db/repeatable/R__7_populate_fee_other_civil.sql
+++ scheme-service/src/main/resources/db/repeatable/R__7_populate_fee_other_civil.sql
@@ -1,3 +1,14 @@
 INSERT INTO fee (fee_code, description, fixed_fee, escape_threshold_limit, fee_scheme_code, calculation_type)
-VALUES ('COM', 'Community Care Legal Help Fixed Fee', 266.00,  798.00, 'COM_FS2013', 'COMMUNITY_CARE')
+VALUES ('CAPA', 'Claims Against Public Authorities Legal Help Fixed Fee', 239.00, 717.00, 'CAPA_FS2013','CLAIMS_PUBLIC_AUTHORITIES'),
+       ('CLIN', 'Clinical Negligence Legal Help Fixed Fee', 195.00, 585.00, 'CLIN_FS2013', 'CLINICAL_NEGLIGENCE'),
+       ('COM', 'Community Care Legal Help Fixed Fee', 266.00, 798.00, 'COM_FS2013', 'COMMUNITY_CARE'),
+       ('DEBT', 'Debt Legal Help Fixed Fee', 180.00, 540.00, 'DEBT_FS2013', 'DEBT'),
+       ('ELA', 'HLPAS Stage One: Early Legal Advice', 157.00, 471.00, 'ELA_FS2024', 'HOUSING_HLPAS'),
+       ('HOUS', 'Housing Fixed Fee', 157.00, 471.00, 'HOUS_FS2013', 'HOUSING'),
+       ('MISCASBI', 'Miscellaneous (ASBI) Legal Help Fixed Fee', 157.00, 471.00, 'MISCASBI_FS2015', 'MISCELLANEOUS'),
+       ('MISCCON', 'Miscellaneous (Consumer) Legal Help Fixed Fee', 159.00, 477.00, 'MISCCON_FS2013', 'MISCELLANEOUS'),
+       ('MISCEMP', 'Miscellaneous (Employment) Legal Help Fixed Fee', 207.00, 621.00, 'MISCEMP_FS2013', 'MISCELLANEOUS'),
+       ('MISCGEN', 'Miscellaneous Legal Help Fixed Fee', 79.00, 237.00, 'MISCGEN_FS2013', 'MISCELLANEOUS'),
+       ('MISCPI', 'Miscellaneous (Personal Injury) Legal Help Fixed Fee', 203.00, 609.00, 'MISCPI_FS2013', 'MISCELLANEOUS'),
+       ('PUB', 'Public Law', 259.00, 777.00, 'PUB_FS2013', 'PUBLIC_LAW')
 ON CONFLICT (fee_code, fee_scheme_code) DO NOTHING;
