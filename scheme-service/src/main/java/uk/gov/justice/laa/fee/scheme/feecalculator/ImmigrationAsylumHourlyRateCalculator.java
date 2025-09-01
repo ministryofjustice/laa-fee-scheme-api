f01aa5b7ad29a2a370b8e7b3208a3f900e66a5f6
Tosin <tosin.yusuf@digital.justice.gov.uk>
2025-09-01 14:24:57 +0100

    LFSP-113: Tidy up

--- scheme-service/src/main/java/uk/gov/justice/laa/fee/scheme/feecalculator/ImmigrationAsylumHourlyRateCalculator.java
+++ scheme-service/src/main/java/uk/gov/justice/laa/fee/scheme/feecalculator/ImmigrationAsylumHourlyRateCalculator.java
@@ -22,6 +22,9 @@ public final class ImmigrationAsylumHourlyRateCalculator {
   private ImmigrationAsylumHourlyRateCalculator() {
   }
 
+  public static final String IAXL = "IAXL";
+  public static final String IMXL = "IMXL";
+
   private static final String WARNING_NET_PROFIT_COSTS = "warning net profit costs"; // @TODO: TBC
   private static final String WARNING_NET_DISBURSEMENTS = "warning net disbursements"; // @TODO: TBC
 
@@ -33,54 +36,60 @@ public final class ImmigrationAsylumHourlyRateCalculator {
    * @return FeeCalculationResponse with calculated fee
    */
   public static FeeCalculationResponse getFee(FeeEntity feeEntity, FeeCalculationRequest feeCalculationRequest) {
-    List<String> warnings = new ArrayList<>();
+    String feeCode = feeEntity.getFeeCode();
+    if (IAXL.equals(feeCode) || IMXL.equals(feeCode)) {
+      List<String> warnings = new ArrayList<>();
 
-    BigDecimal netProfitCosts = toBigDecimal(feeCalculationRequest.getNetProfitCosts());
-    BigDecimal profitCostLimit = feeEntity.getProfitCostLimit();
-    if (netProfitCosts.compareTo(profitCostLimit) > 0
-        && StringUtils.isBlank(feeCalculationRequest.getImmigrationPriorityAuthority())) {
-      netProfitCosts = profitCostLimit;
-      warnings.add(WARNING_NET_PROFIT_COSTS);
+      BigDecimal netProfitCosts = toBigDecimal(feeCalculationRequest.getNetProfitCosts());
+      BigDecimal profitCostLimit = feeEntity.getProfitCostLimit();
+      if (netProfitCosts.compareTo(profitCostLimit) > 0
+          && StringUtils.isBlank(feeCalculationRequest.getImmigrationPriorityAuthority())) {
+        netProfitCosts = profitCostLimit;
+        warnings.add(WARNING_NET_PROFIT_COSTS);
+      }
+
+      BigDecimal netDisbursementAmount = toBigDecimal(feeCalculationRequest.getNetDisbursementAmount());
+      BigDecimal disbursementLimit = feeEntity.getDisbursementLimit();
+      if (netDisbursementAmount.compareTo(disbursementLimit) > 0
+          && StringUtils.isBlank(feeCalculationRequest.getImmigrationPriorityAuthority())) {
+        netDisbursementAmount = disbursementLimit;
+        warnings.add(WARNING_NET_DISBURSEMENTS);
+      }
+
+      BigDecimal jrFormFilling = toBigDecimal(feeCalculationRequest.getJrFormFilling());
+      BigDecimal feeTotal = netProfitCosts.add(jrFormFilling);
+
+      // Apply VAT where applicable
+      LocalDate startDate = feeCalculationRequest.getStartDate();
+      Boolean vatApplicable = feeCalculationRequest.getVatIndicator();
+      BigDecimal calculatedVatAmount = VatUtility.getVatAmount(feeTotal, startDate, vatApplicable);
+
+      BigDecimal disbursementVatAmount = toBigDecimal(feeCalculationRequest.getDisbursementVatAmount());
+
+      BigDecimal finalTotal = feeTotal
+          .add(calculatedVatAmount)
+          .add(netDisbursementAmount)
+          .add(disbursementVatAmount);
+
+      return new FeeCalculationResponse().toBuilder()
+          .feeCode(feeCalculationRequest.getFeeCode())
+          .schemeId(feeEntity.getFeeSchemeCode().getSchemeCode())
+          .warnings(warnings)
+          .feeCalculation(FeeCalculation.builder()
+              .totalAmount(toDouble(finalTotal))
+              .vatIndicator(feeCalculationRequest.getVatIndicator())
+              .vatRateApplied(toDouble(VatUtility.getVatRateForDate(feeCalculationRequest.getStartDate())))
+              .calculatedVatAmount(toDouble(calculatedVatAmount))
+              .disbursementAmount(toDouble(netDisbursementAmount))
+              .disbursementVatAmount(toDouble(disbursementVatAmount))
+              .hourlyTotalAmount(toDouble(feeTotal))
+              .netProfitCostsAmount(toDouble(netProfitCosts))
+              .jrFormFillingAmount(toDouble(jrFormFilling))
+              .build())
+          .build();
+    } else {
+      //@TODO: to be removed once bus rules for all fee codes are implemented
+      throw new IllegalArgumentException("Fee code not supported: " + feeCode);
     }
-
-    BigDecimal netDisbursementAmount = toBigDecimal(feeCalculationRequest.getNetDisbursementAmount());
-    BigDecimal disbursementLimit = feeEntity.getDisbursementLimit();
-    if (netDisbursementAmount.compareTo(disbursementLimit) > 0
-        && StringUtils.isBlank(feeCalculationRequest.getImmigrationPriorityAuthority())) {
-      netDisbursementAmount = disbursementLimit;
-      warnings.add(WARNING_NET_DISBURSEMENTS);
-    }
-
-    BigDecimal jrFormFilling = toBigDecimal(feeCalculationRequest.getJrFormFilling());
-    BigDecimal feeTotal = netProfitCosts.add(jrFormFilling);
-
-    // Apply VAT where applicable
-    LocalDate startDate = feeCalculationRequest.getStartDate();
-    Boolean vatApplicable = feeCalculationRequest.getVatIndicator();
-    BigDecimal calculatedVatAmount = VatUtility.getVatAmount(feeTotal, startDate, vatApplicable);
-
-    BigDecimal disbursementVatAmount = toBigDecimal(feeCalculationRequest.getDisbursementVatAmount());
-
-    BigDecimal finalTotal = feeTotal
-        .add(calculatedVatAmount)
-        .add(netDisbursementAmount)
-        .add(disbursementVatAmount);
-
-    return new FeeCalculationResponse().toBuilder()
-        .feeCode(feeCalculationRequest.getFeeCode())
-        .schemeId(feeEntity.getFeeSchemeCode().getSchemeCode())
-        .warnings(warnings)
-        .feeCalculation(FeeCalculation.builder()
-            .totalAmount(toDouble(finalTotal))
-            .vatIndicator(feeCalculationRequest.getVatIndicator())
-            .vatRateApplied(toDouble(VatUtility.getVatRateForDate(feeCalculationRequest.getStartDate())))
-            .calculatedVatAmount(toDouble(calculatedVatAmount))
-            .disbursementAmount(toDouble(netDisbursementAmount))
-            .disbursementVatAmount(toDouble(disbursementVatAmount))
-            .hourlyTotalAmount(toDouble(feeTotal))
-            .netProfitCostsAmount(toDouble(netProfitCosts))
-            .jrFormFillingAmount(toDouble(jrFormFilling))
-            .build())
-        .build();
   }
 }
