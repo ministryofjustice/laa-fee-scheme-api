cbbb7ef920217912ac30af094ac637073922323f
mukesh-moj <mukesh.kumar@justice.gov.uk>
2025-09-29 09:53:15 +0100

    Files added

--- scheme-service/src/main/resources/db/repeatable/R__11_populate_fee_prison_law.sql
+++ scheme-service/src/main/resources/db/repeatable/R__11_populate_fee_prison_law.sql
@@ -1,100 +1,13 @@
--- Fee Scheme for 2016 'Criminal proceedings - Magistrates court' Undesignated
-INSERT INTO fee
-(fee_code, description, fixed_fee, category_type, fee_type, fee_scheme_code)
-VALUES
-    ('PROE1', 'Representation in the Magistrates Court - category 1A - lower standard fee - undesignated area', 194.68, 'MAGS_COURT_UNDESIGNATED', 'FIXED', 'MAGS_COURT_FS2016'),
-    ('PROE2', 'Representation in the Magistrates Court - category 1B - lower standard fee - undesignated area', 158.27, 'MAGS_COURT_UNDESIGNATED', 'FIXED', 'MAGS_COURT_FS2016'),
-    ('PROE3', 'Representation in the Magistrates Court - category 2 - lower standard fee - undesignated area', 279.45, 'MAGS_COURT_UNDESIGNATED', 'FIXED', 'MAGS_COURT_FS2016'),
-    ('PROF1', 'Representation in the Magistrates Court - category 1A - higher standard fee - undesignated area', 412.30, 'MAGS_COURT_UNDESIGNATED', 'FIXED', 'MAGS_COURT_FS2016'),
-    ('PROF2', 'Representation in the Magistrates Court - category 1B - higher standard fee - undesignated area', 380.70, 'MAGS_COURT_UNDESIGNATED', 'FIXED', 'MAGS_COURT_FS2016'),
-    ('PROF3', 'Representation in the Magistrates Court - category 2 - higher standard fee - undesignated area', 640.94, 'MAGS_COURT_UNDESIGNATED', 'FIXED', 'MAGS_COURT_FS2016'),
-    ('PROJ1', 'Representation in the Magistrates Court - second claim for deferred sentence - category 1A - lower standard fee - undesignated area', 194.68, 'MAGS_COURT_UNDESIGNATED', 'FIXED', 'MAGS_COURT_FS2016'),
-    ('PROJ2', 'Representation in the Magistrates Court - second claim for deferred sentence - category 1B - lower standard fee - undesignated area', 158.27, 'MAGS_COURT_UNDESIGNATED', 'FIXED', 'MAGS_COURT_FS2016'),
-    ('PROJ3', 'Representation in the Magistrates Court - second claim for deferred sentence - category 1A - higher standard fee - undesignated area', 412.30, 'MAGS_COURT_UNDESIGNATED', 'FIXED', 'MAGS_COURT_FS2016'),
-    ('PROJ4', 'Representation in the Magistrates Court - second claim for deferred sentence - category 1B - higher standard fee - undesignated area', 380.70, 'MAGS_COURT_UNDESIGNATED', 'FIXED', 'MAGS_COURT_FS2016')
-ON CONFLICT (fee_code, fee_scheme_code) DO NOTHING;
+-- Fee Codes for Prison Law
 
--- Fee Scheme for 2016 'Criminal proceedings - Magistrates court' Designated
-INSERT INTO fee
-(fee_code, description, fixed_fee, category_type, fee_type, fee_scheme_code)
-VALUES
-    ('PROJ5', 'Representation in the Magistrates Court - second claim for deferred sentence - category 1A - lower standard fee - designated area', 248.71, 'MAGS_COURT_DESIGNATED', 'FIXED', 'MAGS_COURT_FS2016'),
-    ('PROJ6', 'Representation in the Magistrates Court - second claim for deferred sentence - category 1B - lower standard fee - designated area', 202.20, 'MAGS_COURT_DESIGNATED', 'FIXED', 'MAGS_COURT_FS2016'),
-    ('PROJ7', 'Representation in the Magistrates Court - second claim for deferred sentence - category 1A - higher standard fee - designated area', 471.81, 'MAGS_COURT_DESIGNATED', 'FIXED', 'MAGS_COURT_FS2016'),
-    ('PROJ8', 'Representation in the Magistrates Court - second claim for deferred sentence - category 1B - higher standard fee - designated area', 435.64, 'MAGS_COURT_DESIGNATED', 'FIXED', 'MAGS_COURT_FS2016'),
-    ('PROK1', 'Representation in the Magistrates Court - category 1A - lower standard fee - designated area', 248.71, 'MAGS_COURT_DESIGNATED', 'FIXED', 'MAGS_COURT_FS2016'),
-    ('PROK2', 'Representation in the Magistrates Court - category 1B - lower standard fee - designated area', 202.20, 'MAGS_COURT_DESIGNATED', 'FIXED', 'MAGS_COURT_FS2016'),
-    ('PROK3', 'Representation in the Magistrates Court - category 2 - lower standard fee - designated area', 345.34, 'MAGS_COURT_DESIGNATED', 'FIXED', 'MAGS_COURT_FS2016'),
-    ('PROL1', 'Representation in the Magistrates Court - category 1A - higher standard fee - designated area', 471.81, 'MAGS_COURT_DESIGNATED', 'FIXED', 'MAGS_COURT_FS2016'),
-    ('PROL2', 'Representation in the Magistrates Court - category 1B - higher standard fee - designated area', 435.64, 'MAGS_COURT_DESIGNATED', 'FIXED', 'MAGS_COURT_FS2016'),
-    ('PROL3', 'Representation in the Magistrates Court - category 2 - higher standard fee - designated area', 723.35, 'MAGS_COURT_DESIGNATED', 'FIXED', 'MAGS_COURT_FS2016')
-ON CONFLICT (fee_code, fee_scheme_code) DO NOTHING;
-
--- Fee Scheme for 2022 'Criminal proceedings - Magistrates court' Undesignated
-INSERT INTO fee
-(fee_code, description, fixed_fee, category_type, fee_type, fee_scheme_code)
-VALUES
-    ('PROE1', 'Representation in the Magistrates Court - category 1A - lower standard fee - undesignated area', 223.88, 'MAGS_COURT_UNDESIGNATED', 'FIXED', 'MAGS_COURT_FS2022'),
-    ('PROE2', 'Representation in the Magistrates Court - category 1B - lower standard fee - undesignated area', 182.01, 'MAGS_COURT_UNDESIGNATED', 'FIXED', 'MAGS_COURT_FS2022'),
-    ('PROE3', 'Representation in the Magistrates Court - category 2 - lower standard fee - undesignated area', 321.37, 'MAGS_COURT_UNDESIGNATED', 'FIXED', 'MAGS_COURT_FS2022'),
-    ('PROF1', 'Representation in the Magistrates Court - category 1A - higher standard fee - undesignated area', 474.15, 'MAGS_COURT_UNDESIGNATED', 'FIXED', 'MAGS_COURT_FS2022'),
-    ('PROF2', 'Representation in the Magistrates Court - category 1B - higher standard fee - undesignated area', 437.81, 'MAGS_COURT_UNDESIGNATED', 'FIXED', 'MAGS_COURT_FS2022'),
-    ('PROF3', 'Representation in the Magistrates Court - category 2 - higher standard fee - undesignated area', 737.08, 'MAGS_COURT_UNDESIGNATED', 'FIXED', 'MAGS_COURT_FS2022'),
-    ('PROJ1', 'Representation in the Magistrates Court - second claim for deferred sentence - category 1A - lower standard fee - undesignated area', 223.88, 'MAGS_COURT_UNDESIGNATED', 'FIXED', 'MAGS_COURT_FS2022'),
-    ('PROJ2', 'Representation in the Magistrates Court - second claim for deferred sentence - category 1B - lower standard fee - undesignated area', 182.01, 'MAGS_COURT_UNDESIGNATED', 'FIXED', 'MAGS_COURT_FS2022'),
-    ('PROJ3', 'Representation in the Magistrates Court - second claim for deferred sentence - category 1A - higher standard fee - undesignated area', 474.15, 'MAGS_COURT_UNDESIGNATED', 'FIXED', 'MAGS_COURT_FS2022'),
-    ('PROJ4', 'Representation in the Magistrates Court - second claim for deferred sentence - category 1B - higher standard fee - undesignated area', 437.81, 'MAGS_COURT_UNDESIGNATED', 'FIXED', 'MAGS_COURT_FS2022')
-ON CONFLICT (fee_code, fee_scheme_code) DO NOTHING;
-
-
--- Fee Scheme for 2022 'Criminal proceedings - Magistrates court' Designated
-INSERT INTO fee
-(fee_code, description, fixed_fee, category_type, fee_type, fee_scheme_code)
-VALUES
-    ('PROJ5', 'Representation in the Magistrates Court - second claim for deferred sentence - category 1A - lower standard fee - designated area', 286.02, 'MAGS_COURT_DESIGNATED', 'FIXED', 'MAGS_COURT_FS2022'),
-    ('PROJ6', 'Representation in the Magistrates Court - second claim for deferred sentence - category 1B - lower standard fee - designated area', 232.53, 'MAGS_COURT_DESIGNATED', 'FIXED', 'MAGS_COURT_FS2022'),
-    ('PROJ7', 'Representation in the Magistrates Court - second claim for deferred sentence - category 1A - higher standard fee - designated area', 542.58, 'MAGS_COURT_DESIGNATED', 'FIXED', 'MAGS_COURT_FS2022'),
-    ('PROJ8', 'Representation in the Magistrates Court - second claim for deferred sentence - category 1B - higher standard fee - designated area', 500.99, 'MAGS_COURT_DESIGNATED', 'FIXED', 'MAGS_COURT_FS2022'),
-    ('PROK1', 'Representation in the Magistrates Court - category 1A - lower standard fee - designated area', 286.02, 'MAGS_COURT_DESIGNATED', 'FIXED', 'MAGS_COURT_FS2022'),
-    ('PROK2', 'Representation in the Magistrates Court - category 1B - lower standard fee - designated area', 232.53, 'MAGS_COURT_DESIGNATED', 'FIXED', 'MAGS_COURT_FS2022'),
-    ('PROK3', 'Representation in the Magistrates Court - category 2 - lower standard fee - designated area', 397.14, 'MAGS_COURT_DESIGNATED', 'FIXED', 'MAGS_COURT_FS2022'),
-    ('PROL1', 'Representation in the Magistrates Court - category 1A - higher standard fee - designated area', 542.58, 'MAGS_COURT_DESIGNATED', 'FIXED', 'MAGS_COURT_FS2022'),
-    ('PROL2', 'Representation in the Magistrates Court - category 1B - higher standard fee - designated area', 500.99, 'MAGS_COURT_DESIGNATED', 'FIXED', 'MAGS_COURT_FS2022'),
-    ('PROL3', 'Representation in the Magistrates Court - category 2 - higher standard fee - designated area', 831.85, 'MAGS_COURT_DESIGNATED', 'FIXED', 'MAGS_COURT_FS2022')
-ON CONFLICT (fee_code, fee_scheme_code) DO NOTHING;
-
--- Fee Scheme for 2024 'Criminal proceedings - Youth court' Undesignated
-INSERT INTO fee
-(fee_code, description, fixed_fee, category_type, fee_type, fee_scheme_code)
-VALUES
-    ('YOUE1', 'Youth Representation Order - category 1A - lower standard fee - undesignated area', 822.47, 'YOUTH_COURT_UNDESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024'),
-    ('YOUE2', 'Youth Representation Order - category 1B - lower standard fee - undesignated area', 182.01, 'YOUTH_COURT_UNDESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024'),
-    ('YOUE3', 'Youth Representation Order - category 2A - lower standard fee - undesignated area', 919.96, 'YOUTH_COURT_UNDESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024'),
-    ('YOUE4', 'Youth Representation Order - category 2B - lower standard fee - undesignated area', 321.37, 'YOUTH_COURT_UNDESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024'),
-    ('YOUF1', 'Youth Representation Order - category 1A - higher standard fee - undesignated area', 1072.74, 'YOUTH_COURT_UNDESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024'),
-    ('YOUF2', 'Youth Representation Order - category 1B - higher standard fee - undesignated area', 437.81, 'YOUTH_COURT_UNDESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024'),
-    ('YOUF3', 'Youth Representation Order - category 2A - higher standard fee - undesignated area', 1335.67, 'YOUTH_COURT_UNDESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024'),
-    ('YOUF4', 'Youth Representation Order - category 2B - higher standard fee - undesignated area', 737.08, 'YOUTH_COURT_UNDESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024'),
-    ('YOUX1', 'Youth Representation Order - second claim for deferred sentence - category 1A - lower standard fee - undesignated area', 822.47, 'YOUTH_COURT_UNDESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024'),
-    ('YOUX2', 'Youth Representation Order - second claim for deferred sentence - category 1B - lower standard fee - undesignated area', 182.01, 'YOUTH_COURT_UNDESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024'),
-    ('YOUX3', 'Youth Representation Order - second claim for deferred sentence - category 1A - higher standard fee - undesignated area', 1072.74, 'YOUTH_COURT_UNDESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024'),
-    ('YOUX4', 'Youth Representation Order - second claim for deferred sentence - category 1B - higher standard fee - undesignated area', 437.81, 'YOUTH_COURT_UNDESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024')
-ON CONFLICT (fee_code, fee_scheme_code) DO NOTHING;
-
--- Fee Scheme for 2024 'Criminal proceedings - Youth court' Designated
-INSERT INTO fee
-(fee_code, description, fixed_fee, category_type, fee_type, fee_scheme_code)
-VALUES
-    ('YOUK1', 'Youth Representation Order - category 1A - lower standard fee - designated area', 884.61, 'YOUTH_COURT_DESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024'),
-    ('YOUK2', 'Youth Representation Order - category 1B - lower standard fee - designated area', 232.53, 'YOUTH_COURT_DESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024'),
-    ('YOUK3', 'Youth Representation Order - category 2A - lower standard fee - designated area', 995.73, 'YOUTH_COURT_DESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024'),
-    ('YOUK4', 'Youth Representation Order - category 2B - lower standard fee - designated area', 397.14, 'YOUTH_COURT_DESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024'),
-    ('YOUL1', 'Youth Representation Order - category 1A - higher standard fee - designated area', 1141.17, 'YOUTH_COURT_DESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024'),
-    ('YOUL2', 'Youth Representation Order - category 1B - higher standard fee - designated area', 500.99, 'YOUTH_COURT_DESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024'),
-    ('YOUL3', 'Youth Representation Order - category 2A - higher standard fee - designated area', 1430.44, 'YOUTH_COURT_DESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024'),
-    ('YOUL4', 'Youth Representation Order - category 2B - higher standard fee - designated area', 831.85, 'YOUTH_COURT_DESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024'),
-    ('YOUY1', 'Youth Representation Order - second claim for deferred sentence - category 1A - lower standard fee - designated area', 884.61, 'YOUTH_COURT_DESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024'),
-    ('YOUY2', 'Youth Representation Order - second claim for deferred sentence - category 1B - lower standard fee - designated area', 232.53, 'YOUTH_COURT_DESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024'),
-    ('YOUY3', 'Youth Representation Order - second claim for deferred sentence - category 1A - higher standard fee - designated area', 1141.17, 'YOUTH_COURT_DESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024'),
-    ('YOUY4', 'Youth Representation Order - second claim for deferred sentence - category 1B - higher standard fee - designated area', 500.99, 'YOUTH_COURT_DESIGNATED', 'FIXED', 'YOUTH_COURT_FS2024')
+INSERT INTO fee (fee_code,description,fee_scheme_code,fixed_fee,escape_threshold_limit,category_type,total_limit,fee_type) VALUES
+  ('PRIA','Free Standing Advice and Assistance','PRISON_FS2016',200.75,602.25,'PRISON_LAW',NULL,'FIXED'),
+  ('PRIB1','Disciplinary Cases – Advocacy Assistance - lower standard fee','PRISON_FS2016',203.93,NULL,'PRISON_LAW_LOWER_STANDARD',357.06,'FIXED'),
+  ('PRIC1','Parole Board Cases – Advocacy Assistance - lower standard fee','PRISON_FS2016',437.21,NULL,'PRISON_LAW_LOWER_STANDARD',933.93,'FIXED'),
+  ('PRID1','Advocacy Assistance at Sentence Reviews - lower standard fee','PRISON_FS2016',437.21,NULL,'PRISON_LAW_LOWER_STANDARD',357.06,'FIXED'),
+  ('PRIE1','Advocacy Assistance at Parole Board Reconsideration Hearings - lower standard fee','PRISON_FS2016',203.93,NULL,'PRISON_LAW_LOWER_STANDARD',933.93,'FIXED'),
+  ('PRIB2','Disciplinary Cases – Advocacy Assistance - higher standard fee','PRISON_FS2016',564.16,1691.69,'PRISON_LAW_HIGHER_STANDARD',NULL,'FIXED'),
+  ('PRIC2','Parole Board Cases – Advocacy Assistance - higher standard fee','PRISON_FS2016',1454.44,4362.54,'PRISON_LAW_HIGHER_STANDARD',NULL,'FIXED'),
+  ('PRID2','Advocacy Assistance at Sentence Reviews - higher standard fee','PRISON_FS2016',564.16,1691.69,'PRISON_LAW_HIGHER_STANDARD',NULL,'FIXED'),
+  ('PRIE2','Advocacy Assistance at Parole Board Reconsideration Hearings - higher standard fee','PRISON_FS2016',1454.44,4362.54,'PRISON_LAW_HIGHER_STANDARD',NULL,'FIXED')
 ON CONFLICT (fee_code, fee_scheme_code) DO NOTHING;
