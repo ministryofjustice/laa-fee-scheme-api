44343665b13a2af7ff4e0d850d574c9caf604021
Tosin <tosin.yusuf@digital.justice.gov.uk>
2025-09-29 15:39:28 +0100

    LFSP-79: Tidy up

--- scheme-service/src/main/resources/db/repeatable/R__13_populate_fee_associated_civil.sql
+++ scheme-service/src/main/resources/db/repeatable/R__13_populate_fee_associated_civil.sql
@@ -2,7 +2,7 @@
 INSERT INTO fee
 (fee_code, description, fixed_fee, escape_threshold_limit, category_type, fee_type, fee_scheme_code)
 VALUES
-    ('ASMS', 'Legal Help and Associated Civil Work – Miscellaneous', 79, 237,'ASSOCIATED_CIVIL', 'FIXED', 'ASSOC_FS2016'),
-    ('ASPL', 'Legal Help and Associated Civil Work – Public Law', 259, 777,'ASSOCIATED_CIVIL', 'FIXED', 'ASSOC_FS2016'),
-    ('ASAS', 'Part 1 injunction Anti-Social Behaviour Crime and Policing Act 2014', 157, 471,'ASSOCIATED_CIVIL', 'FIXED', 'ASSOC_FS2016')
+    ('ASMS', 'Legal Help and Associated Civil Work – Miscellaneous', 79, 237, 'ASSOCIATED_CIVIL', 'FIXED', 'ASSOC_FS2016'),
+    ('ASPL', 'Legal Help and Associated Civil Work – Public Law', 259, 777, 'ASSOCIATED_CIVIL', 'FIXED', 'ASSOC_FS2016'),
+    ('ASAS', 'Part 1 injunction Anti-Social Behaviour Crime and Policing Act 2014', 157, 471, 'ASSOCIATED_CIVIL', 'FIXED', 'ASSOC_FS2016')
 ON CONFLICT (fee_code, fee_scheme_code) DO NOTHING;
