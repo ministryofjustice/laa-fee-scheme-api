01b3eb4fcdad16216a34b65d093a7c2134e23f54
Tosin <tosin.yusuf@digital.justice.gov.uk>
2025-10-01 16:49:13 +0100

    LFSP-105: Remove script file

--- scheme-service/src/main/resources/db/repeatable/R__12_populate_fee_associated_civil.sql
+++
@@ -1,8 +0,0 @@
--- Fee Scheme for 2016 'Associated Civil' category
-INSERT INTO fee
-(fee_code, description, fixed_fee, escape_threshold_limit, category_type, fee_type, fee_scheme_code)
-VALUES
-    ('ASMS', 'Legal Help and Associated Civil Work – Miscellaneous', 79, 237,'ASSOCIATED_CIVIL', 'FIXED', 'ASSOC_FS2016'),
-    ('ASPL', 'Legal Help and Associated Civil Work – Public Law', 259, 777,'ASSOCIATED_CIVIL', 'FIXED', 'ASSOC_FS2016'),
-    ('ASAS', 'Part 1 injunction Anti-Social Behaviour Crime and Policing Act 2014', 157, 471,'ASSOCIATED_CIVIL', 'FIXED', 'ASSOC_FS2016')
-ON CONFLICT (fee_code, fee_scheme_code) DO NOTHING;
