4638cea7f1852fd1b5d900051148bd8dd7e6a361
drummondjm <mark.drummond@justice.gov.uk>
2025-10-06 09:13:15 +0100

    initial spike

--- scheme-service/src/main/java/uk/gov/justice/laa/fee/scheme/controller/GetSubmissionData.java
+++ scheme-service/src/main/java/uk/gov/justice/laa/fee/scheme/controller/GetSubmissionData.java
@@ -5,13 +5,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.justice.laa.fee.scheme.mapstruct.SubmissionDataDto;
 import uk.gov.justice.laa.fee.scheme.mapstruct.SubmissionJsonData;
-import uk.gov.justice.laa.fee.scheme.model.FeeDetailsResponse;
 import uk.gov.justice.laa.fee.scheme.service.SubmissionDataService;
 
 
