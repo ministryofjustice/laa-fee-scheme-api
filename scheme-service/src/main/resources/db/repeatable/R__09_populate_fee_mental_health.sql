389116ee6accf8b8bebfc6ad289038ee52258c7f
mukesh-moj <mukesh.kumar@justice.gov.uk>
2025-10-15 16:11:24 +0100

    SQL scripts added

--- scheme-service/src/main/resources/db/repeatable/R__09_populate_fee_mental_health.sql
+++ scheme-service/src/main/resources/db/repeatable/R__09_populate_fee_mental_health.sql
@@ -7,7 +7,8 @@ VALUES ('MHL01', 'Non-Mental Health Tribunal Fee', 263.00, 759.00, NULL, 'MHL_FS
        ('MHL06', 'Mental Health Tribunal Fee - Levels 2 and 3', 615.00, 1845.00, 117.00, 'MHL_FS2013', 'MENTAL_HEALTH', 'FIXED'),
        ('MHL07', 'Mental Health Tribunal Fee - Level 3 only', 294.00, 882.00, 117.00, 'MHL_FS2013', 'MENTAL_HEALTH', 'FIXED'),
        ('MHL08', 'Mental Health Tribunal Fee - Levels 1 and 3', 423.00, 1269.00, 117.00, 'MHL_FS2013', 'MENTAL_HEALTH', 'FIXED'),
-       ('MHL10', 'Mental Health Tribunal Fee - Level 1 (Rule 11(7)(a) cases where a patient has not engaged with the provider)', 129.00, NULL,NULL, 'MHL_FS2013', 'MENTAL_HEALTH', 'FIXED')
+       ('MHL10', 'Mental Health Tribunal Fee - Level 1 (Rule 11(7)(a) cases where a patient has not engaged with the provider)', 129.00, NULL,NULL, 'MHL_FS2013', 'MENTAL_HEALTH', 'FIXED'),
+       ('MHLDIS', 'Mental Health - Interim Claim for Disbursements', NULL, NULL,NULL, 'MHL_FS2013', 'MENTAL_HEALTH', 'DISB_ONLY')
 ON CONFLICT (fee_code, fee_scheme_code) DO UPDATE
 SET
     fee_type = EXCLUDED.fee_type;
