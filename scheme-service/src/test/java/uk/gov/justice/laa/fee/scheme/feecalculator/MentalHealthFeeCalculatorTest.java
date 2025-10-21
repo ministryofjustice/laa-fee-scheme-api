3aa375f23c1393869f0c84b6d6b1c6c39c8dc55f
mukesh-moj <mukesh.kumar@justice.gov.uk>
2025-10-21 09:34:15 +0100

    LFSP-200 - Implement business rules for disbursements in Mental health and Education (#149)
    * SQL scripts added
    
    * Files modified to handle Disbursement claims
    
    * review comments addressed

---
+++ scheme-service/src/test/java/uk/gov/justice/laa/fee/scheme/feecalculator/MentalHealthFeeCalculatorTest.java
@@ -0,0 +1,132 @@
+package uk.gov.justice.laa.fee.scheme.feecalculator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static uk.gov.justice.laa.fee.scheme.enums.CategoryType.MENTAL_HEALTH;
+import static uk.gov.justice.laa.fee.scheme.enums.FeeType.DISB_ONLY;
+import static uk.gov.justice.laa.fee.scheme.enums.FeeType.FIXED;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.justice.laa.fee.scheme.entity.FeeEntity;
+import uk.gov.justice.laa.fee.scheme.entity.FeeSchemesEntity;
+import uk.gov.justice.laa.fee.scheme.enums.CategoryType;
+import uk.gov.justice.laa.fee.scheme.feecalculator.disbursement.MentalHealthDisbursementOnlyCalculator;
+import uk.gov.justice.laa.fee.scheme.feecalculator.fixed.MentalHealthFixedFeeCalculator;
+import uk.gov.justice.laa.fee.scheme.model.FeeCalculation;
+import uk.gov.justice.laa.fee.scheme.model.FeeCalculationRequest;
+import uk.gov.justice.laa.fee.scheme.model.FeeCalculationResponse;
+
+@ExtendWith(MockitoExtension.class)
+class MentalHealthFeeCalculatorTest {
+
+  @InjectMocks
+  MentalHealthFeeCalculator mentalHealthFeeCalculator;
+
+  @Mock
+  MentalHealthFixedFeeCalculator mentalHealthFixedFeeCalculator;
+
+  @Mock
+  MentalHealthDisbursementOnlyCalculator mentalHealthDisbursementOnlyCalculator;
+
+  @Test
+  void getFee_whenMentalHealthFeeFixed_shouldReturnFeeCalculationResponse() {
+
+    FeeCalculationRequest feeCalculationRequest = FeeCalculationRequest.builder()
+        .feeCode("MHL03")
+        .startDate(LocalDate.of(2017, 7, 29))
+        .vatIndicator(true)
+        .netDisbursementAmount(50.50)
+        .disbursementVatAmount(20.15)
+        .build();
+
+    FeeEntity feeEntity = FeeEntity.builder()
+        .feeCode("MHL03")
+        .feeScheme(FeeSchemesEntity.builder().schemeCode("MHL_FS2013").build())
+        .fixedFee(new BigDecimal("321.0"))
+        .categoryType(MENTAL_HEALTH)
+        .feeType(FIXED)
+        .build();
+
+    FeeCalculation expectedCalculation = FeeCalculation.builder()
+        .totalAmount(311.32)
+        .vatIndicator(Boolean.TRUE)
+        .vatRateApplied(20.0)
+        .calculatedVatAmount(40.11)
+        .disbursementAmount(50.5)
+        .disbursementVatAmount(20.15)
+        .fixedFeeAmount(200.56)
+        .calculatedVatAmount(40.11)
+        .build();
+
+    FeeCalculationResponse expectedResponse = FeeCalculationResponse.builder()
+        .feeCode("MHL03")
+        .schemeId("MHL_FS2013")
+        .escapeCaseFlag(false)
+        .feeCalculation(expectedCalculation)
+        .build();
+
+    when(mentalHealthFixedFeeCalculator.calculate(feeCalculationRequest, feeEntity)).thenReturn(expectedResponse);
+
+    FeeCalculationResponse result = mentalHealthFeeCalculator.calculate(feeCalculationRequest, feeEntity);
+
+    assertThat(result).isNotNull();
+    assertThat(result.getFeeCode()).isEqualTo("MHL03");
+    assertThat(result.getFeeCalculation()).isNotNull();
+    assertThat(result.getFeeCalculation().getTotalAmount()).isEqualTo(311.32);
+  }
+
+  @Test
+  void getFee_whenMentalHealthFeeDisbursement_shouldReturnFeeCalculationResponse() {
+
+    FeeCalculationRequest feeCalculationRequest = FeeCalculationRequest.builder()
+        .feeCode("EDUDIS")
+        .startDate(LocalDate.of(2017, 7, 29))
+        .vatIndicator(true)
+        .netDisbursementAmount(50.50)
+        .disbursementVatAmount(20.15)
+        .build();
+
+    FeeEntity feeEntity = FeeEntity.builder()
+        .feeCode("EDUDIS")
+        .feeScheme(FeeSchemesEntity.builder().schemeCode("MHL_FS2013").build())
+        .categoryType(MENTAL_HEALTH)
+        .feeType(DISB_ONLY)
+        .disbursementLimit(new BigDecimal("400.00"))
+        .build();
+
+    FeeCalculation expectedCalculation = FeeCalculation.builder()
+        .totalAmount(311.32)
+        .build();
+
+    FeeCalculationResponse expectedResponse = FeeCalculationResponse.builder()
+        .feeCode("EDUDIS")
+        .schemeId("MHL_FS2013")
+        .escapeCaseFlag(false)
+        .feeCalculation(expectedCalculation)
+        .build();
+
+    when(mentalHealthDisbursementOnlyCalculator.calculate(feeCalculationRequest, feeEntity)).thenReturn(expectedResponse);
+
+    FeeCalculationResponse result = mentalHealthFeeCalculator.calculate(feeCalculationRequest, feeEntity);
+
+    assertThat(result).isNotNull();
+    assertThat(result.getFeeCode()).isEqualTo("EDUDIS");
+    assertThat(result.getFeeCalculation()).isNotNull();
+    assertThat(result.getFeeCalculation().getTotalAmount()).isEqualTo(311.32);
+  }
+
+  @Test
+  void getSupportedCategories_shouldReturnMentalHealthCategory() {
+    Set<CategoryType> result = mentalHealthFeeCalculator.getSupportedCategories();
+
+    assertThat(result).isEqualTo(Set.of(MENTAL_HEALTH));
+  }
+
+}
