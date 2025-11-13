23498703da46724dbbcd45a5ff1603dd314a64ee
Tosin <tosin.yusuf@digital.justice.gov.uk>
2025-11-13 12:38:09 +0000

    Tidy up

--- scheme-service/src/integrationtest/java/uk/gov/justice/laa/fee/scheme/validation/FeeCalculationValidationIntegrationTest.java
+++ scheme-service/src/integrationtest/java/uk/gov/justice/laa/fee/scheme/validation/FeeCalculationValidationIntegrationTest.java
@@ -8,7 +8,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
