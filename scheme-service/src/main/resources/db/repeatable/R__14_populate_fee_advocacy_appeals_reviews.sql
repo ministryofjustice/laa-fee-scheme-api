2e35d2ea5c59ba11737d304e7eb6485e6c5e5beb
drummondjm <mark.drummond@justice.gov.uk>
2025-11-27 15:32:28 +0000

    initial changes for lfsp 261

--- scheme-service/src/main/resources/db/repeatable/R__14_populate_fee_advocacy_appeals_reviews.sql
+++ scheme-service/src/main/resources/db/repeatable/R__14_populate_fee_advocacy_appeals_reviews.sql
@@ -15,3 +15,13 @@ VALUES
     ('APPA', 314.81, 'AAR_FS2022'),
     ('APPB', 524.69, 'AAR_FS2022')
 ON CONFLICT (fee_code, fee_scheme_code) DO NOTHING;
+
+-- Fee Scheme for 2025 uplift 'Advocacy Assistance in the Crown Court or Appeals & Reviews' category
+INSERT INTO fee
+(fee_code, upper_cost_limit, fee_scheme_code)
+VALUES
+    ('PROH1', 1574.06, 'AAR_FS2025'),
+    ('PROH2', 1574.06, 'AAR_FS2025'),
+    ('APPA', 314.81, 'AAR_FS2025'),
+    ('APPB', 524.69, 'AAR_FS2025')
+    ON CONFLICT (fee_code, fee_scheme_code) DO NOTHING;
