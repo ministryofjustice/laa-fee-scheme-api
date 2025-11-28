78a31c0d7fa67bd43bfb35332e4db14225f8fc99
Tosin <tosin.yusuf@digital.justice.gov.uk>
2025-11-28 11:10:29 +0000

    LFSP-262: Added prison law fee uplifts

--- scheme-service/src/main/resources/db/repeatable/R__01_populate_fee_scheme.sql
+++ scheme-service/src/main/resources/db/repeatable/R__01_populate_fee_scheme.sql
@@ -140,13 +140,13 @@ VALUES ('POL_FS2016', 'Police Station Work 2016', '2016-04-01', NULL),
        ('POL_FS2024', 'Police Station Work 2024', '2024-12-06', NULL),
        ('POL_FS2025', 'Police Station Work 2025', '2025-12-22', NULL)
 ON CONFLICT (scheme_code) DO NOTHING;
-/** '2028-08-04' set temporarily, Valid from date needs to be updated once it is confirmed **/
 
 -- Fee Scheme for 'Prison Law' category
 INSERT INTO fee_schemes (scheme_code, scheme_name, valid_from, valid_to)
-VALUES
-    ('PRISON_FS2016', 'Prison Law 2016', '2016-04-01', NULL)
-    ON CONFLICT (scheme_code) DO NOTHING;
+VALUES ('PRISON_FS2016', 'Prison Law Fee Scheme 2016', '2016-04-01', '2025-12-21'),
+       ('PRISON_FS2025', 'Prison Law Fee Scheme 2025', '2025-12-22', NULL)
+ON CONFLICT (scheme_code) DO UPDATE SET
+    valid_to = EXCLUDED.valid_to;
 
 -- Fee Scheme for 'Public Law' category
 INSERT INTO fee_schemes (scheme_code, scheme_name, valid_from, valid_to)
--- scheme-service/src/main/resources/db/repeatable/R__12_populate_fee_prison_law.sql
+++ scheme-service/src/main/resources/db/repeatable/R__12_populate_fee_prison_law.sql
@@ -1,11 +1,23 @@
-INSERT INTO fee (fee_code,fee_scheme_code,fixed_fee,escape_threshold_limit,total_limit) VALUES
-  ('PRIA','PRISON_FS2016',200.75,602.25,NULL),
-  ('PRIB1','PRISON_FS2016',203.93,NULL,357.06),
-  ('PRIC1','PRISON_FS2016',437.21,NULL,933.93),
-  ('PRID1','PRISON_FS2016',437.21,NULL,357.06),
-  ('PRIE1','PRISON_FS2016',203.93,NULL,933.93),
-  ('PRIB2','PRISON_FS2016',564.16,1691.69,NULL),
-  ('PRIC2', 'PRISON_FS2016',1454.44,4362.54,NULL),
-  ('PRID2','PRISON_FS2016',564.16,1691.69,NULL),
-  ('PRIE2','PRISON_FS2016',1454.44,4362.54,NULL)
+INSERT INTO fee (fee_code, fee_scheme_code, fixed_fee, escape_threshold_limit, total_limit)
+VALUES ('PRIA', 'PRISON_FS2016', 200.75, 602.25, NULL),
+       ('PRIB1', 'PRISON_FS2016', 203.93, NULL, 357.06),
+       ('PRIB2', 'PRISON_FS2016', 564.16, 1691.69, NULL),
+       ('PRIC1', 'PRISON_FS2016', 437.21, NULL, 933.93),
+       ('PRIC2', 'PRISON_FS2016', 1454.44, 4362.54, NULL),
+       ('PRID1', 'PRISON_FS2016', 437.21, NULL, 357.06),
+       ('PRID2', 'PRISON_FS2016', 564.16, 1691.69, NULL),
+       ('PRIE1', 'PRISON_FS2016', 203.93, NULL, 933.93),
+       ('PRIE2', 'PRISON_FS2016', 1454.44, 4362.54, NULL)
+ON CONFLICT (fee_code, fee_scheme_code) DO NOTHING;
+
+INSERT INTO fee (fee_code, fee_scheme_code, fixed_fee, escape_threshold_limit, total_limit)
+VALUES ('PRIA', 'PRISON_FS2025', 248.93, 746.79, NULL),
+       ('PRIB1', 'PRISON_FS2025', 252.87, NULL, 442.75),
+       ('PRIB2', 'PRISON_FS2025', 699.56, 2097.70, NULL),
+       ('PRIC1', 'PRISON_FS2025', 542.14, NULL, 1158.07),
+       ('PRIC2', 'PRISON_FS2025', 1803.51, 5409.55, NULL),
+       ('PRID1', 'PRISON_FS2025', 252.87, NULL, 442.75),
+       ('PRID2', 'PRISON_FS2025', 699.56, 2097.70, NULL),
+       ('PRIE1', 'PRISON_FS2025', 542.14, NULL, 1158.07),
+       ('PRIE2', 'PRISON_FS2025', 1803.51, 5409.55, NULL)
 ON CONFLICT (fee_code, fee_scheme_code) DO NOTHING;
