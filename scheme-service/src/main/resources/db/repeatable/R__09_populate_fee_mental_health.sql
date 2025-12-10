e2dad8148e52a3c1fa4e0a727f6c02f403dc0d13
Tosin <tosin.yusuf@justice.gov.uk>
2025-12-10 11:12:52 +0000

    LFSP-279: Fixed MH01 fixed fee in db script (#238)

--- scheme-service/src/main/resources/db/repeatable/R__09_populate_fee_mental_health.sql
+++ scheme-service/src/main/resources/db/repeatable/R__09_populate_fee_mental_health.sql
@@ -1,12 +1,12 @@
 INSERT INTO fee (fee_code, fixed_fee, escape_threshold_limit, adjorn_hearing_bolt_on, fee_scheme_code)
-VALUES ('MHL01', 263.00, 759.00, NULL, 'MHL_FS2013'),
-       ('MHL02', 129.00, 387.00, 117.00,'MHL_FS2013'),
-       ('MHL03', 450.00, 1350.00, 117.00,'MHL_FS2013'),
+VALUES ('MHL01', 253.00, 759.00, NULL, 'MHL_FS2013'),
+       ('MHL02', 129.00, 387.00, 117.00, 'MHL_FS2013'),
+       ('MHL03', 450.00, 1350.00, 117.00, 'MHL_FS2013'),
        ('MHL04', 744.00, 2232.00, 117.00, 'MHL_FS2013'),
-       ('MHL05', 321.00, 963.00, 117.00,'MHL_FS2013'),
+       ('MHL05', 321.00, 963.00, 117.00, 'MHL_FS2013'),
        ('MHL06', 615.00, 1845.00, 117.00, 'MHL_FS2013'),
        ('MHL07', 294.00, 882.00, 117.00, 'MHL_FS2013'),
        ('MHL08', 423.00, 1269.00, 117.00, 'MHL_FS2013'),
-       ('MHL10', 129.00, NULL,NULL, 'MHL_FS2013'),
-       ('MHLDIS', NULL, NULL,NULL, 'MHL_DISB_FS2013')
+       ('MHL10', 129.00, NULL, NULL, 'MHL_FS2013'),
+       ('MHLDIS', NULL, NULL, NULL, 'MHL_DISB_FS2013')
 ON CONFLICT (fee_code, fee_scheme_code) DO NOTHING;
