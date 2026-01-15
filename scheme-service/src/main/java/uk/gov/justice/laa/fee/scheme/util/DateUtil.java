01aa6d4227e33f139dbe786021ac4c1a134dc881
drummondjm <mark.drummond@justice.gov.uk>
2026-01-15 15:18:07 +0000

    update parsing logic to pivot on base year

--- scheme-service/src/main/java/uk/gov/justice/laa/fee/scheme/util/DateUtil.java
+++ scheme-service/src/main/java/uk/gov/justice/laa/fee/scheme/util/DateUtil.java
@@ -2,7 +2,6 @@ package uk.gov.justice.laa.fee.scheme.util;
 
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.ChronoField;
 
@@ -11,32 +10,31 @@ import java.time.temporal.ChronoField;
  */
 
 public final class DateUtil {
+  private static final int PIVOT_YEAR = 50;
+  private static final int NINETEEN_HUNDRED = 1900;
+  private static final int TWENTY_HUNDRED = 2000;
 
-  private DateUtil() {
-  }
+  private DateUtil() {}
 
   /**
-   * Converts a Date String value to LocalDate.
-   * if UFN year greater than 50, parse as 1900, else 2000
+   * Converts a date string in the format ddMMyy.
+   * Century is determined by the year value:
+   * - 50-99 = 1900s
+   * - 00-49 = 2000s
    *
    * @param inputStr the Date String value to convert
    * @return the converted LocalDate
    */
   public static @NotNull LocalDate toLocalDate(String inputStr) {
     String strDate = inputStr.substring(0, 6); // ddMMyy
-
     int ufnYear = Integer.parseInt(strDate.substring(4, 6));
-    int pivot = 50;
-    int baseYear = (ufnYear >= pivot) ? 1900 : 2000;
+    int baseYear = (ufnYear >= PIVOT_YEAR) ? NINETEEN_HUNDRED : TWENTY_HUNDRED;
 
-    // Define formatter for ddMMyy
-    DateTimeFormatter formatter =
+    return LocalDate.parse(strDate,
         new DateTimeFormatterBuilder()
             .appendPattern("ddMM")
             .appendValueReduced(ChronoField.YEAR, 2, 2, baseYear)
-            .toFormatter();
-    // Parse into LocalDate
-    return LocalDate.parse(strDate, formatter);
+            .toFormatter()
+    );
   }
-
 }
