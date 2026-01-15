b15a02664206d38a1470caff62599845ad824df8
drummondjm <93932689+drummondjm@users.noreply.github.com>
2026-01-15 16:24:04 +0000

    LFSP-302 update ufn parsing (#259)
    * update parsing logic to pivot on base year
    
    * update parsing logic to pivot on base year
    
    * additional test

--- scheme-service/src/main/java/uk/gov/justice/laa/fee/scheme/util/DateUtil.java
+++ scheme-service/src/main/java/uk/gov/justice/laa/fee/scheme/util/DateUtil.java
@@ -2,32 +2,39 @@ package uk.gov.justice.laa.fee.scheme.util;
 
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
 
 /**
  * Utility class for Date string conversions.
  */
 
 public final class DateUtil {
+  private static final int PIVOT_YEAR = 50;
+  private static final int NINETEEN_HUNDRED = 1900;
+  private static final int TWENTY_HUNDRED = 2000;
 
-  private static final String DATE_FORMAT = "ddMMyy";
-
-  private DateUtil() {
-  }
+  private DateUtil() {}
 
   /**
-   * Converts a Date String value to LocalDate.
+   * Converts a date string in the format ddMMyy.
+   * Century is determined by the year value:
+   * - 50-99 = 1900s
+   * - 00-49 = 2000s
    *
    * @param inputStr the Date String value to convert
    * @return the converted LocalDate
    */
-  @NotNull
   public static @NotNull LocalDate toLocalDate(String inputStr) {
-    String strDate = inputStr.substring(0, 6); // "120325"
-    // Define formatter for ddMMyy
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
-    // Parse into LocalDate
-    return LocalDate.parse(strDate, formatter);
-  }
+    String strDate = inputStr.substring(0, 6); // ddMMyy
+    int ufnYear = Integer.parseInt(strDate.substring(4, 6));
+    int baseYear = (ufnYear >= PIVOT_YEAR) ? NINETEEN_HUNDRED : TWENTY_HUNDRED;
 
+    return LocalDate.parse(strDate,
+        new DateTimeFormatterBuilder()
+            .appendPattern("ddMM")
+            .appendValueReduced(ChronoField.YEAR, 2, 2, baseYear)
+            .toFormatter()
+    );
+  }
 }
