9b6c74e4b33bf7345baa1220890fecaa1e67b9fc
drummondjm <mark.drummond@justice.gov.uk>
2026-02-11 15:40:12 +0000

    update throwing of ERRCRM12

--- scheme-service/src/main/java/uk/gov/justice/laa/fee/scheme/service/FeeCodeConstants.java
+++ scheme-service/src/main/java/uk/gov/justice/laa/fee/scheme/service/FeeCodeConstants.java
@@ -15,7 +15,4 @@ public final class FeeCodeConstants {
 
   public static final String FEE_CODE_PROD = "PROD";
 
-  public static final Pattern REP_ORDER_DATE_PATTERN = Pattern.compile(
-      "^(PRO[EFKLV][1-4]|PROJ[1-8]|YOU[EFXKLY][1-4]|APP[AB]|PROW)$");
-
 }
