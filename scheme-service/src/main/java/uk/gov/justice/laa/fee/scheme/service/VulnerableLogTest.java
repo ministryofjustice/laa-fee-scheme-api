1f4c985c72a75068c1d60a9df4a1fcc0b8fd2dbe
drummondjm <mark.drummond@justice.gov.uk>
2026-03-13 15:13:52 +0000

    https://dsdmoj.atlassian.net/browse/LFSP-370

--- scheme-service/src/main/java/uk/gov/justice/laa/fee/scheme/service/VulnerableLogTest.java
+++ scheme-service/src/main/java/uk/gov/justice/laa/fee/scheme/service/VulnerableLogTest.java
@@ -3,6 +3,9 @@ package uk.gov.justice.laa.fee.scheme.service;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
 
+/**
+ * Calculate fee for a given fee calculation request.
+ */
 public class VulnerableLogTest {
   private static final Logger logger = (Logger) LogManager.getLogger(VulnerableLogTest.class);
 
