d0251eb3f8436181f7d7ee24a3e16487f9d16926
drummondjm <mark.drummond@justice.gov.uk>
2026-03-13 15:02:20 +0000

    https://dsdmoj.atlassian.net/browse/LFSP-370

---
+++ scheme-service/src/main/java/uk/gov/justice/laa/fee/scheme/service/VulnerableLogTest.java
@@ -0,0 +1,13 @@
+package uk.gov.justice.laa.fee.scheme.service;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+
+public class VulnerableLogTest {
+  private static final Logger logger = (Logger) LogManager.getLogger(VulnerableLogTest.class);
+
+  public void testLogging() {
+    logger.info("Testing vulnerable Log4j 2.14.1");
+    logger.error("This should trigger CVE-2021-44228 detection");
+  }
+}
