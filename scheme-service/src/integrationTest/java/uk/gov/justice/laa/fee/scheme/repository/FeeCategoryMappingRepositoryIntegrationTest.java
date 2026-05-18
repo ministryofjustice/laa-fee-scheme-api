bf9b9a4ddb72c24ac634c27f88a450685fbb3a91
joshwoodmoj <joshua.wood2@justice.gov.uk>
2026-05-18 15:56:12 +0100

    Added integration tests.

--- scheme-service/src/integrationTest/java/uk/gov/justice/laa/fee/scheme/repository/FeeCategoryMappingRepositoryIntegrationTest.java
+++ scheme-service/src/integrationTest/java/uk/gov/justice/laa/fee/scheme/repository/FeeCategoryMappingRepositoryIntegrationTest.java
@@ -2,11 +2,13 @@ package uk.gov.justice.laa.fee.scheme.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import uk.gov.justice.laa.fee.scheme.entity.FeeCategoryMappingEntity;
+import uk.gov.justice.laa.fee.scheme.enums.AreaOfLawType;
 import uk.gov.justice.laa.fee.scheme.enums.FeeType;
 import uk.gov.justice.laa.fee.scheme.postgrestestcontainer.PostgresContainerTestBase;
 
@@ -29,7 +31,8 @@ class FeeCategoryMappingRepositoryIntegrationTest extends PostgresContainerTestB
 
     FeeCategoryMappingEntity feeCategoryMappingEntity = result.get();
     assertThat(feeCategoryMappingEntity.getCategoryOfLawType().getCode()).isEqualTo("IMMAS");
-    assertThat(feeCategoryMappingEntity.getFeeCode().getFeeDescription()).isEqualTo("Standard Fee - Asylum CLR  (2a)");
+    assertThat(feeCategoryMappingEntity.getFeeCode().getFeeDescription())
+        .isEqualTo("Standard Fee - Asylum CLR  (2a)");
     assertThat(feeCategoryMappingEntity.getFeeCode().getFeeType()).isEqualTo(FeeType.FIXED);
   }
 
@@ -39,5 +42,27 @@ class FeeCategoryMappingRepositoryIntegrationTest extends PostgresContainerTestB
     Optional<FeeCategoryMappingEntity> result = repository.findByFeeCodeFeeCode(feeCode);
     assertThat(result).isEmpty();
   }
-}
 
+  @Test
+  void shouldReturnFeeCategoryMappingsWhenAreaOfLawExists() {
+
+    List<FeeCategoryMappingEntity> result =
+        repository.findByCategoryOfLawTypeAreaOfLawTypeCode(AreaOfLawType.LEGAL_HELP);
+
+    assertThat(result).isNotEmpty();
+
+    FeeCategoryMappingEntity entity = result.getFirst();
+
+    assertThat(entity.getCategoryOfLawType()).isNotNull();
+    assertThat(entity.getCategoryOfLawType().getAreaOfLawType()).isNotNull();
+
+    assertThat(entity.getCategoryOfLawType().getAreaOfLawType().getCode())
+        .isEqualTo(AreaOfLawType.LEGAL_HELP);
+
+    assertThat(entity.getFeeCode()).isNotNull();
+
+    assertThat(entity.getFeeCode().getFeeDescription()).isNotBlank();
+
+    assertThat(entity.getFeeCode().getFeeType()).isIn(FeeType.FIXED, FeeType.HOURLY);
+  }
+}
