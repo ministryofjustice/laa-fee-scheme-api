94f198a624d14052090c743d363daa6e40326ed6
joshwoodmoj <joshua.wood2@justice.gov.uk>
2026-07-29 09:52:28 +0100

    Updated error message and updated relevant tests.

--- scheme-service/src/main/java/uk/gov/justice/laa/fee/scheme/enums/ErrorType.java
+++ scheme-service/src/main/java/uk/gov/justice/laa/fee/scheme/enums/ErrorType.java
@@ -14,8 +14,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ErrorType {
   ERR_ALL_FEE_CODE("ERRALL1", "Enter a valid Fee Code."),
-  ERR_CIVIL_START_DATE("ERRCIV1", "Fee Code is not valid for the Case Start Date."),
-  ERR_CIVIL_START_DATE_TOO_OLD("ERRCIV2", "Case Start Date is too far in the past."),
+  ERR_CIVIL_START_DATE("ERRCIV1", "Fee Code and Case Start Date combination is not valid. Check both fields and resubmit your claim."),
+  ERR_CIVIL_START_DATE_TOO_OLD("ERRCIV2", "Cases started before 1st April 2013 cannot be accepted. Check Case Start Date and resubmit."),
   ERR_CRIME_POLICE_SCHEME_ID("ERRCRM4", "Enter a valid Scheme ID."),
   ERR_CRIME_POLICE_STATION_ID("ERRCRM3", "Enter a valid Police station ID, Court ID, or Prison ID."),
   ERR_CRIME_PREORDER_COVER_UPPER_LIMIT("ERRCRM10", "Net Cost is more than the Upper Cost Limitation."),
