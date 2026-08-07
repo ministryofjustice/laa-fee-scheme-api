79f15e874e78d1e544f89eb2f5b5c6d084c324be
joshwoodmoj <joshua.wood2@justice.gov.uk>
2026-08-07 12:55:02 +0100

    Error messages updated and relevant tests updated with new error message.

--- scheme-service/src/test/java/uk/gov/justice/laa/fee/scheme/service/CrimeFeeValidationServiceTest.java
+++ scheme-service/src/test/java/uk/gov/justice/laa/fee/scheme/service/CrimeFeeValidationServiceTest.java
@@ -372,7 +372,7 @@ class CrimeFeeValidationServiceTest {
     assertThatThrownBy(() -> crimeFeeValidationService.getValidFeeEntity(feeEntityList, feeCalculationRequest))
         .isInstanceOf(ValidationException.class)
         .hasFieldOrPropertyWithValue("error", ERR_CRIME_REP_ORDER_DATE)
-        .hasMessage("ERRCRM12 - Fee Code is not valid for the Representation Order Date provided.");
+        .hasMessage("ERRCRM12 - Fee code and representation order date are incompatible. Check both fields and resubmit.");
   }
 
   @Test
