569895cd0be771620f8b6fdd51da7f64b7e1c80c
joshwoodmoj <joshua.wood2@justice.gov.uk>
2026-09-02 11:18:07 +0100

    Added examples of how to test functionality using feature flags.

--- scheme-service/src/test/java/uk/gov/justice/laa/fee/scheme/feecalculator/util/EducationFeeCalculatorTest.java
+++ scheme-service/src/test/java/uk/gov/justice/laa/fee/scheme/feecalculator/util/EducationFeeCalculatorTest.java
@@ -2,6 +2,8 @@ package uk.gov.justice.laa.fee.scheme.feecalculator.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static uk.gov.justice.laa.fee.scheme.enums.CategoryType.EDUCATION;
 import static uk.gov.justice.laa.fee.scheme.enums.CategoryType.MENTAL_HEALTH;
@@ -12,11 +14,16 @@ import static uk.gov.justice.laa.fee.scheme.enums.FeeType.HOURLY;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Set;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.LoggerFactory;
 import uk.gov.justice.laa.fee.scheme.config.FeatureFlagsConfig;
 import uk.gov.justice.laa.fee.scheme.entity.FeeEntity;
 import uk.gov.justice.laa.fee.scheme.entity.FeeSchemesEntity;
@@ -80,10 +87,13 @@ class EducationFeeCalculatorTest {
         .feeCalculation(expectedCalculation)
         .build();
 
+    when(featureFlagsConfig.getIsFeatureEnabled()).thenReturn(true);
     when(educationFixedFeeCalculator.calculate(feeCalculationRequest, feeEntity)).thenReturn(expectedResponse);
 
     FeeCalculationResponse result = educationFeeCalculator.calculate(feeCalculationRequest, feeEntity);
 
+    verify(featureFlagsConfig).getIsFeatureEnabled();
+    verify(educationFixedFeeCalculator).calculate(feeCalculationRequest, feeEntity);
     assertThat(result).isNotNull();
     assertThat(result.getFeeCode()).isEqualTo("EDUFIN");
     assertThat(result.getFeeCalculation()).isNotNull();
@@ -120,10 +130,13 @@ class EducationFeeCalculatorTest {
         .feeCalculation(expectedCalculation)
         .build();
 
+    when(featureFlagsConfig.getIsFeatureEnabled()).thenReturn(false);
     when(educationDisbursementOnlyCalculator.calculate(feeCalculationRequest, feeEntity)).thenReturn(expectedResponse);
 
     FeeCalculationResponse result = educationFeeCalculator.calculate(feeCalculationRequest, feeEntity);
 
+    verify(featureFlagsConfig).getIsFeatureEnabled();
+    verify(educationDisbursementOnlyCalculator).calculate(feeCalculationRequest, feeEntity);
     assertThat(result).isNotNull();
     assertThat(result.getFeeCode()).isEqualTo("EDUDIS");
     assertThat(result.getFeeCalculation()).isNotNull();
@@ -141,6 +154,8 @@ class EducationFeeCalculatorTest {
         .feeType(HOURLY)
         .build();
 
+    when(featureFlagsConfig.getIsFeatureEnabled()).thenReturn(false);
+
     assertThatThrownBy(() -> educationFeeCalculator.calculate(feeCalculationRequest, feeEntity))
         .isInstanceOf(UnsupportedOperationException.class)
         .hasMessage("Hourly rate fee is not supported for Education category.");
@@ -151,5 +166,105 @@ class EducationFeeCalculatorTest {
     Set<CategoryType> result = educationFeeCalculator.getSupportedCategories();
 
     assertThat(result).isEqualTo(Set.of(EDUCATION));
+  }
+
+  @Test
+  void calculate_whenFeatureEnabledAndFeeTypeIsFixed_shouldOnlyUseFixedCalculator() {
+    FeeCalculationRequest feeCalculationRequest = FeeCalculationRequest.builder()
+        .feeCode("EDUFIX")
+        .build();
+
+    FeeEntity feeEntity = FeeEntity.builder()
+        .feeCode("EDUFIX")
+        .feeType(FIXED)
+        .build();
+
+    FeeCalculationResponse expectedResponse = FeeCalculationResponse.builder()
+        .feeCode("EDUFIX")
+        .build();
+
+    when(featureFlagsConfig.getIsFeatureEnabled()).thenReturn(true);
+    when(educationFixedFeeCalculator.calculate(feeCalculationRequest, feeEntity)).thenReturn(expectedResponse);
+
+    FeeCalculationResponse result = educationFeeCalculator.calculate(feeCalculationRequest, feeEntity);
+
+    assertThat(result).isEqualTo(expectedResponse);
+    verify(featureFlagsConfig).getIsFeatureEnabled();
+    verify(educationFixedFeeCalculator).calculate(feeCalculationRequest, feeEntity);
+    verifyNoInteractions(educationDisbursementOnlyCalculator);
+  }
+
+  @Test
+  void calculate_whenFeatureFlagIsNull_shouldStillUseDisbursementCalculatorForDisbursementFeeType() {
+    FeeCalculationRequest feeCalculationRequest = FeeCalculationRequest.builder()
+        .feeCode("EDUDIS")
+        .build();
+
+    FeeEntity feeEntity = FeeEntity.builder()
+        .feeCode("EDUDIS")
+        .feeType(DISB_ONLY)
+        .build();
+
+    FeeCalculationResponse expectedResponse = FeeCalculationResponse.builder()
+        .feeCode("EDUDIS")
+        .build();
+
+    when(featureFlagsConfig.getIsFeatureEnabled()).thenReturn(null);
+    when(educationDisbursementOnlyCalculator.calculate(feeCalculationRequest, feeEntity)).thenReturn(expectedResponse);
+
+    FeeCalculationResponse result = educationFeeCalculator.calculate(feeCalculationRequest, feeEntity);
+
+    assertThat(result).isEqualTo(expectedResponse);
+    verify(featureFlagsConfig).getIsFeatureEnabled();
+    verify(educationDisbursementOnlyCalculator).calculate(feeCalculationRequest, feeEntity);
+    verifyNoInteractions(educationFixedFeeCalculator);
+  }
+
+  @Test
+  void calculate_whenFeatureEnabled_logsEnabledConditionalMessage() {
+    FeeCalculationRequest feeCalculationRequest = FeeCalculationRequest.builder().feeCode("EDUFIX").build();
+    FeeEntity feeEntity = FeeEntity.builder().feeCode("EDUFIX").feeType(FIXED).build();
+    FeeCalculationResponse expectedResponse = FeeCalculationResponse.builder().feeCode("EDUFIX").build();
+
+    Logger logger = (Logger) LoggerFactory.getLogger(EducationFeeCalculator.class);
+    ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+    listAppender.start();
+    logger.addAppender(listAppender);
+
+    when(featureFlagsConfig.getIsFeatureEnabled()).thenReturn(true);
+    when(educationFixedFeeCalculator.calculate(feeCalculationRequest, feeEntity)).thenReturn(expectedResponse);
+
+    educationFeeCalculator.calculate(feeCalculationRequest, feeEntity);
+
+    assertThat(listAppender.list).anyMatch(event ->
+        event.getLevel() == Level.INFO
+            && event.getFormattedMessage().contains("Feature enabled. Using conditional statement"));
+
+    listAppender.stop();
+    logger.detachAppender(listAppender);
+  }
+
+  @Test
+  void calculate_whenFeatureDisabled_logsDisabledConditionalMessage() {
+    FeeCalculationRequest feeCalculationRequest = FeeCalculationRequest.builder().feeCode("EDUDIS").build();
+    FeeEntity feeEntity = FeeEntity.builder().feeCode("EDUDIS").feeType(DISB_ONLY).build();
+    FeeCalculationResponse expectedResponse = FeeCalculationResponse.builder().feeCode("EDUDIS").build();
+
+    Logger logger = (Logger) LoggerFactory.getLogger(EducationFeeCalculator.class);
+    ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+    listAppender.start();
+    logger.addAppender(listAppender);
+
+    when(featureFlagsConfig.getIsFeatureEnabled()).thenReturn(false);
+    when(educationDisbursementOnlyCalculator.calculate(feeCalculationRequest, feeEntity)).thenReturn(expectedResponse);
+
+    educationFeeCalculator.calculate(feeCalculationRequest, feeEntity);
+
+    assertThat(listAppender.list).anyMatch(event ->
+        event.getLevel() == Level.INFO
+            && event.getFormattedMessage().contains("Feature disabled. Using conditional statement"));
+
+    listAppender.stop();
+    logger.detachAppender(listAppender);
   }
 }
